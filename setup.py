6329b120dc2d1644b361a78491abc956812846dd
Daniel Matuschek <daniel@matuschek.net>
2016-08-29 14:30:49 +0200

    V 0.1.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='miflora',
-      version='0.1.3',
+      version='0.1.4',
       description='Library to read data from Mi Flora sensor',
       url='https://github.com/open-homeautomation/miflora',
       author='Daniel Matuschek',
