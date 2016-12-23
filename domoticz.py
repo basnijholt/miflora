c72d89b842eb66b7b31288b38de35c905b8141d1
Tristan Crispijn <Tristan@MacBook-Pro.local>
2016-12-24 00:45:24 +0100

    Domoticz Script

---
+++ domoticz.py
@@ -0,0 +1,54 @@
+import urllib.request
+import base64
+from miflora.miflora_poller import MiFloraPoller, \
+    MI_CONDUCTIVITY, MI_MOISTURE, MI_LIGHT, MI_TEMPERATURE, MI_BATTERY
+
+# Settings for the domoticz server
+domoticzserver   = "127.0.0.1:8080"
+domoticzusername = ""
+domoticzpassword = ""
+
+# Sensor IDs
+idx_temp  = "138"
+idx_lux   = "140"
+idx_moist = "139"
+idx_cond  = "141"
+
+############
+
+base64string = base64.encodestring(('%s:%s' % (domoticzusername, domoticzpassword)).encode()).decode().replace('\n', '')
+
+def domoticzrequest (url):
+  request = urllib.request.Request(url)
+  request.add_header("Authorization", "Basic %s" % base64string)
+  response = urllib.request.urlopen(request)
+  return response.read()
+
+poller = MiFloraPoller("C4:7C:8D:62:42:88")
+#print("Getting data from Mi Flora")
+#print("FW: {}".format(poller.firmware_version()))
+#print("Name: {}".format(poller.name()))
+#print("Temperature: {}".format(poller.parameter_value("temperature")))
+#print("Moisture: {}".format(poller.parameter_value(MI_MOISTURE)))
+#print("Light: {}".format(poller.parameter_value(MI_LIGHT)))
+#print("Conductivity: {}".format(poller.parameter_value(MI_CONDUCTIVITY)))
+#print("Battery: {}".format(poller.parameter_value(MI_BATTERY)))
+
+val_bat  = "{}".format(poller.parameter_value(MI_BATTERY))
+
+# Update temp
+val_temp = "{}".format(poller.parameter_value("temperature"))
+domoticzrequest("http://" + domoticzserver + "/json.htm?type=command&param=udevice&idx=" + idx_temp + "&nvalue=0&svalue=" + val_temp + "&battery=" + val_bat)
+
+# Update lux
+val_lux = "{}".format(poller.parameter_value(MI_LIGHT))
+domoticzrequest("http://" + domoticzserver + "/json.htm?type=command&param=udevice&idx=" + idx_lux + "&svalue=" + val_lux + "&battery=" + val_bat)
+
+# Update moisture
+val_moist = "{}".format(poller.parameter_value(MI_MOISTURE))
+domoticzrequest("http://" + domoticzserver + "/json.htm?type=command&param=udevice&idx=" + idx_moist + "&nvalue=" + val_moist + "&battery=" + val_bat)
+
+# Update conductivity
+val_cond = "{}".format(poller.parameter_value(MI_CONDUCTIVITY))
+domoticzrequest("http://" + domoticzserver + "/json.htm?type=command&param=udevice&idx=" + idx_cond + "&svalue=" + val_cond + "&battery=" + val_bat)
+
