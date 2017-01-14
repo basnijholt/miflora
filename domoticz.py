c2cd4b8abc45a78220b67ea1282f38f5adf928d6
Tristan Crispijn <Tristan@MacBook-Pro.local>
2017-01-14 11:18:58 +0100

    .

--- domoticz.py
+++ domoticz.py
@@ -42,7 +42,7 @@ def update(address,idx_moist,idx_temp,idx_lux,idx_cond):
     print("Temperature: {}°C".format(poller.parameter_value("temperature")))
     print("Moisture: {}%".format(poller.parameter_value(MI_MOISTURE)))
     print("Light: {} lux".format(poller.parameter_value(MI_LIGHT)))
-    print("Fertility: {} uS/cm".format())
+    print("Fertility: {} uS/cm".format(poller.parameter_value(MI_CONDUCTIVITY)))
     print("Battery: {}%".format(poller.parameter_value(MI_BATTERY)))
 
     val_bat  = "{}".format(poller.parameter_value(MI_BATTERY))
