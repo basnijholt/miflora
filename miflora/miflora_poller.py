b7d000aa7110845ef15826b7caa07db23e9dc95f
Christian Kühnel <christian.kuehnel@gmail.com>
2017-03-20 19:39:39 +0100

    ordered methods according to control flow, public methods first cleanup of unused or duplicate members

--- miflora/miflora_poller.py
+++ miflora/miflora_poller.py
@@ -7,7 +7,6 @@ No other operating systems are supported at the moment
 """
 
 from datetime import datetime, timedelta
-from threading import Lock
 import logging
 import time
 from bluepy.btle import Peripheral, BTLEException
@@ -24,11 +23,8 @@ INVALID_DATA = b'\xaa\xbb\xcc\xdd\xee\xff\x99\x88wf\x00\x00\x00\x00\x00\x00'
 """Configure how long data is cached before new values are
 retrieved from the sensor.
 """
-UPDATE_INTERVAL = 5*60
 
 LOGGER = logging.getLogger(__name__)
-
-LOCK = Lock()
 
 
 class MiFloraPoller(object):
@@ -42,13 +38,9 @@ class MiFloraPoller(object):
         """
         self._mac = mac
         self._adapter = adapter
-        self._cache = None
         self._cache_timeout = timedelta(seconds=cache_timeout)
         self._last_read = None
-        self._fw_last_read = datetime.now()
-        self.retries = retries
-        self.ble_timeout = 10
-        self.lock = Lock()
+        self._retries = retries
         self._firmware_version = None
         self._battery = None
         self._name = None
@@ -57,16 +49,53 @@ class MiFloraPoller(object):
         self._moisture = None
         self._conductivity = None
 
-    def _fetch_name(self, peripheral):
-        """
-        Fetch the name of the sensor.
-        """
-        byte_array = self._retry(peripheral.readCharacteristic, [0x03])
-        self._name = byte_array.decode('ascii')
+    def battery_level(self):
+        """Return the battery level."""
+        self._fill_cache()
+        return self._battery
 
-    def _fill_cache(self,read_cached=True):
+    def firmware_version(self):
+        """ Return the firmware version. """
+        self._fill_cache()
+        return self._firmware_version
+
+    def name(self):
+        """Return the name of the sensor."""
+        self._fill_cache()
+        return self._name
+
+    def parameter_value(self, parameter, read_cached=True):
+        """
+        Return a value of one of the monitored paramaters.
+
+        This method will try to retrieve the data from cache and only
+        request it by bluetooth if no cached value is stored or the cache is
+        expired.
+        This behaviour can be overwritten by the "read_cached" parameter.
+        """
+        self._fill_cache(read_cached)
+        if parameter == MI_BATTERY:
+            return self.battery_level()
+        elif parameter == MI_CONDUCTIVITY:
+            return self._conductivity
+        elif parameter == MI_MOISTURE:
+            return self._moisture
+        elif parameter == MI_TEMPERATURE:
+            return self._temperature
+        elif parameter == MI_LIGHT:
+            return self._brightness
+        raise Exception('unknown parameter %s', parameter)
+
+    def _fill_cache(self, read_cached=True):
+        """Fetch new data from the sensor.
+
+        This will only update the data if:
+        - there are no previous measurements or
+        - read_cached is False or
+        - the measurements are older than 5 minutes
+        """
         if self._last_read is None or \
-               (self._last_read + timedelta(seconds=UPDATE_INTERVAL)) <= datetime.now() or \
+                (self._last_read + self._cache_timeout) <= datetime.now() or \
                 not read_cached:
             # TODO: pick the right adapter when creating the Peripheral
             peripheral = self._retry(Peripheral, [self._mac])
@@ -78,31 +107,24 @@ class MiFloraPoller(object):
             peripheral.disconnect()
             self._last_read = datetime.now()
 
+    def _fetch_name(self, peripheral):
+        """Fetch the name of the sensor."""
+        byte_array = self._retry(peripheral.readCharacteristic, [0x03])
+        self._name = byte_array.decode('ascii')
+
     def _fetch_version_battery(self, peripheral):
+        """Fetch the version number and battery level from the sensor."""
         result = self._retry(peripheral.readCharacteristic, [0x38])
         self._decode_characteristic_38(result)
 
     def _fetch_measurements(self, peripheral):
+        """Fetch the measurements from the sensor."""
         if self._firmware_version >= "2.6.6":
             self._retry(peripheral.writeCharacteristic, [0x33, bytes([0xA0, 0x1F]), True])
         result = self._retry(peripheral.readCharacteristic, [0x35])
-        LOGGER.debug('Raw data for char 0x35: %s', self._format_bytes(result))
-
         if result == INVALID_DATA:
             raise Exception('Received invalid data from the sensor')
         self._decode_characteristic_35(result)
-
-    def battery_level(self):
-        self._fill_cache()
-        return self._battery
-
-    def firmware_version(self):
-        """ Return the firmware version. """
-        self._fill_cache()
-        return self._firmware_version
-
-    def name(self):
-        return self._name
 
     def _decode_characteristic_38(self, byte_array):
         """Perform byte magic when decoding the data from the sensor."""
@@ -125,44 +147,21 @@ class MiFloraPoller(object):
         self._moisture = int.from_bytes(result[7:8], byteorder=BYTEORDER)
         self._conductivity = int.from_bytes(result[8:10], byteorder=BYTEORDER)
 
+        LOGGER.debug('Raw data for char 0x35: %s', self._format_bytes(result))
         LOGGER.debug('temp: %f', self._temperature)
         LOGGER.debug('brightness: %d', self._brightness)
         LOGGER.debug('conductivity: %d', self._conductivity)
         LOGGER.debug('moisture: %d', self._moisture)
 
-    def parameter_value(self, parameter, read_cached=True):
-        """
-        Return a value of one of the monitored paramaters.
-
-        This method will try to retrieve the data from cache and only
-        request it by bluetooth if no cached value is stored or the cache is
-        expired.
-        This behaviour can be overwritten by the "read_cached" parameter.
-        """
-
-        self._fill_cache(read_cached)
-        if parameter == MI_BATTERY:
-            return self.battery_level()
-        elif parameter == MI_CONDUCTIVITY:
-            return self._conductivity
-        elif parameter == MI_MOISTURE:
-            return self._moisture
-        elif parameter == MI_TEMPERATURE:
-            return self._temperature
-        elif parameter == MI_LIGHT:
-            return self._brightness
-        raise Exception('unknown parameter %s', parameter)
-
-    @staticmethod
-    def _retry(func, args, num_tries=5, sleep_time=0.5):
+    def _retry(self, func, args, sleep_time=0.5):
         """Retry calling a function on Exception."""
-        for i in range(0, num_tries):
+        for i in range(0, self._retries):
             try:
                 return func(*args)
             except BTLEException as exception:
-                LOGGER.info("function %s failed (try %d of %d)", func, i+1, num_tries)
+                LOGGER.info("function %s failed (try %d of %d)", func, i+1, self._retries)
                 time.sleep(sleep_time * (2 ^ i))
-                if i == num_tries - 1:
+                if i == self._retries - 1:
                     LOGGER.error('retry finally failed!')
                     raise exception
                 else:
