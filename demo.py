141e0ae888ecdc01f0e29f96bb7108d8afe31b36
Christian Kühnel <christian.kuehnel@gmail.com>
2017-09-21 14:22:57 +0200

    Added check for format of MAC address, as proposed by @ThomDietrich

--- demo.py
+++ demo.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
 import argparse
+import re
 from miflora.miflora_poller import MiFloraPoller, \
     MI_CONDUCTIVITY, MI_MOISTURE, MI_LIGHT, MI_TEMPERATURE, MI_BATTERY
 
 
+def valid_miflora_mac(mac, pat=re.compile(r"C4:7C:8D:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}")):
+    if not pat.match(mac):
+        raise argparse.ArgumentTypeError('The MAC address "{}" seems to be in the wrong format'.format(mac))
+    return mac
+
 parser = argparse.ArgumentParser()
-parser.add_argument('mac')
+parser.add_argument('mac',type=valid_miflora_mac)
 args = parser.parse_args()
 
 poller = MiFloraPoller(args.mac)
