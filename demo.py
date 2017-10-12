ac7e28ca4bd599738523ccffce480df6256e4df7
ChristianKuehnel <christian.kuehnel@gmail.com>
2017-10-12 22:56:48 +0200

    Added MAC address as command line parameter for demo.py.  (#38)
    * Added MAC address as command line parameter for demo.py. This simplyfies testing the library.
    
    * Added check for format of MAC address, as proposed by @ThomDietrich

--- demo.py
+++ demo.py
@@ -1,11 +1,25 @@
+#!/usr/bin/env python3
+
+import argparse
+import re
 from miflora.miflora_poller import MiFloraPoller, \
     MI_CONDUCTIVITY, MI_MOISTURE, MI_LIGHT, MI_TEMPERATURE, MI_BATTERY
 
-poller = MiFloraPoller("C4:7C:8D:60:8F:E6")
+
+def valid_miflora_mac(mac, pat=re.compile(r"C4:7C:8D:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}")):
+    if not pat.match(mac):
+        raise argparse.ArgumentTypeError('The MAC address "{}" seems to be in the wrong format'.format(mac))
+    return mac
+
+parser = argparse.ArgumentParser()
+parser.add_argument('mac',type=valid_miflora_mac)
+args = parser.parse_args()
+
+poller = MiFloraPoller(args.mac)
 print("Getting data from Mi Flora")
 print("FW: {}".format(poller.firmware_version()))
 print("Name: {}".format(poller.name()))
-print("Temperature: {}".format(poller.parameter_value("temperature")))
+print("Temperature: {}".format(poller.parameter_value(MI_TEMPERATURE)))
 print("Moisture: {}".format(poller.parameter_value(MI_MOISTURE)))
 print("Light: {}".format(poller.parameter_value(MI_LIGHT)))
 print("Conductivity: {}".format(poller.parameter_value(MI_CONDUCTIVITY)))
