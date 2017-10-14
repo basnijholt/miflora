9e8e4eb004e021206309885c0ee6831314886619
Christian Kühnel <christian.kuehnel@gmail.com>
2017-10-14 15:52:40 +0200

    added more documentation

--- test/integration_tests/test_everything.py
+++ test/integration_tests/test_everything.py
@@ -5,14 +5,15 @@ from miflora.miflora_poller import (MiFloraPoller, MI_CONDUCTIVITY,
 
 
 class TestEverything(unittest.TestCase):
-    """End to End test case.
-
-    This check if we can successfully get some data from a real sensor. This test requires bluetooth hardware and a
-    real sensor close by.
-    """
+    """End to End test cases for MiFlora."""
 
     @pytest.mark.usefixtures("mac")
     def test_everything(self):
+        """Test reading data from a sensor
+
+        This check if we can successfully get some data from a real sensor. This test requires bluetooth hardware and a
+        real sensor close by.
+        """
         assert hasattr(self, "mac")
         poller = MiFloraPoller(self.mac)
         self.assertIsNotNone(poller.firmware_version())
