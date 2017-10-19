3d19c91a1d76067563856b436181884dd0701684
Christian Kühnel <christian.kuehnel@gmail.com>
2017-10-19 23:24:13 +0200

    fixed flake8 warnings

--- test/integration_tests/test_demo.py
+++ test/integration_tests/test_demo.py
@@ -12,11 +12,11 @@ class TestDemoPy(unittest.TestCase):
     @pytest.mark.usefixtures("mac")
     def test_bluepy(self):
         self.assertIsNotNone(self.mac)
-        cmd='./demo.py --backend bluepy {}'.format(self.mac)
+        cmd = './demo.py --backend bluepy {}'.format(self.mac)
         subprocess.check_call(cmd, shell=True, cwd=self.root_dir)
 
     @pytest.mark.usefixtures("mac")
     def test_gatttool(self):
         self.assertIsNotNone(self.mac)
-        cmd='./demo.py --backend gatttool {}'.format(self.mac)
+        cmd = './demo.py --backend gatttool {}'.format(self.mac)
         subprocess.check_call(cmd, shell=True, cwd=self.root_dir)
