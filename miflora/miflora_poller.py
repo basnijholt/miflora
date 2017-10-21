c5a4f90763f43f390a35697ccfd01eb208a05277
Christian Kühnel <christian.kuehnel@gmail.com>
2017-10-21 11:23:41 +0200

    miflora_poller: fixed flake8 warnings, added logging, made constants private

--- miflora/miflora_poller.py
+++ miflora/miflora_poller.py
@@ -7,11 +7,11 @@ import logging
 from threading import Lock
 from miflora.backends import BluetoothInterface
 
-HANDLE_READ_VERSION_BATTERY = 0x38
-HANDLE_READ_NAME = 0x03
-HANDLE_READ_SENSOR_DATA = 0x35
-HANDLE_WRITE_MODE_CHANGE = 0x33
-DATA_MODE_CHANGE = bytes([0xA0, 0x1F])
+_HANDLE_READ_VERSION_BATTERY = 0x38
+_HANDLE_READ_NAME = 0x03
+_HANDLE_READ_SENSOR_DATA = 0x35
+_HANDLE_WRITE_MODE_CHANGE = 0x33
+_DATA_MODE_CHANGE = bytes([0xA0, 0x1F])
 
 MI_TEMPERATURE = "temperature"
 MI_LIGHT = "light"
@@ -19,7 +19,7 @@ MI_MOISTURE = "moisture"
 MI_CONDUCTIVITY = "conductivity"
 MI_BATTERY = "battery"
 
-LOGGER = logging.getLogger(__name__)
+_LOGGER = logging.getLogger(__name__)
 
 
 class MiFloraPoller(object):
@@ -45,17 +45,17 @@ class MiFloraPoller(object):
         self.battery = None
 
     def name(self):
-        """
-        Return the name of the sensor.
-        """
+        """Return the name of the sensor."""
         with self._bt_interface.connect(self._mac) as connection:
-            name = connection.read_handle(HANDLE_READ_NAME)
+            name = connection.read_handle(_HANDLE_READ_NAME)
 
         if not name:
-            raise IOError("Could not read data from Mi Flora sensor %s" % (self._mac))
+            raise IOError("Could not read data from Mi Flora sensor %s" % self._mac)
         return ''.join(chr(n) for n in name)
 
     def fill_cache(self):
+        """Fill the cache with new data from the sensor."""
+        _LOGGER.debug('Filling cache with new sensor data.')
         firmware_version = self.firmware_version()
         with self._bt_interface.connect(self._mac) as connection:
             if not firmware_version:
@@ -65,12 +65,14 @@ class MiFloraPoller(object):
                 return
 
             if firmware_version >= "2.6.6":
-                if not connection.write_handle(HANDLE_WRITE_MODE_CHANGE, DATA_MODE_CHANGE):
+                if not connection.write_handle(_HANDLE_WRITE_MODE_CHANGE, _DATA_MODE_CHANGE):
                     # If a sensor doesn't work, wait 5 minutes before retrying
                     self._last_read = datetime.now() - self._cache_timeout + \
                         timedelta(seconds=300)
                     return
-            self._cache = connection.read_handle(HANDLE_READ_SENSOR_DATA)
+            self._cache = connection.read_handle(_HANDLE_READ_SENSOR_DATA)
+            _LOGGER.debug('Received result for handle %s: %s',
+                          _HANDLE_READ_SENSOR_DATA, self._format_bytes(self._cache))
             self._check_data()
             if self._cache is not None:
                 self._last_read = datetime.now()
@@ -80,8 +82,7 @@ class MiFloraPoller(object):
                     timedelta(seconds=300)
 
     def battery_level(self):
-        """
-        Return the battery level.
+        """Return the battery level.
 
         The battery level is updated when reading the firmware version. This
         is done only once every 24h
@@ -90,12 +91,14 @@ class MiFloraPoller(object):
         return self.battery
 
     def firmware_version(self):
-        """ Return the firmware version. """
+        """Return the firmware version."""
         if (self._firmware_version is None) or \
                 (datetime.now() - timedelta(hours=24) > self._fw_last_read):
             self._fw_last_read = datetime.now()
             with self._bt_interface.connect(self._mac) as connection:
-                res = connection.read_handle(HANDLE_READ_VERSION_BATTERY)
+                res = connection.read_handle(_HANDLE_READ_VERSION_BATTERY)
+                _LOGGER.debug('Received result for handle %s: %s',
+                              _HANDLE_READ_VERSION_BATTERY, self._format_bytes(res))
             if res is None:
                 self.battery = 0
                 self._firmware_version = None
@@ -105,15 +108,13 @@ class MiFloraPoller(object):
         return self._firmware_version
 
     def parameter_value(self, parameter, read_cached=True):
-        """
-        Return a value of one of the monitored paramaters.
+        """Return a value of one of the monitored paramaters.
 
         This method will try to retrieve the data from cache and only
         request it by bluetooth if no cached value is stored or the cache is
         expired.
         This behaviour can be overwritten by the "read_cached" parameter.
         """
-
         # Special handling for battery attribute
         if parameter == MI_BATTERY:
             return self.battery_level()
@@ -125,14 +126,14 @@ class MiFloraPoller(object):
                     (datetime.now() - self._cache_timeout > self._last_read):
                 self.fill_cache()
             else:
-                LOGGER.debug("Using cache (%s < %s)",
-                             datetime.now() - self._last_read,
-                             self._cache_timeout)
+                _LOGGER.debug("Using cache (%s < %s)",
+                              datetime.now() - self._last_read,
+                              self._cache_timeout)
 
         if self._cache and (len(self._cache) == 16):
             return self._parse_data()[parameter]
         else:
-            raise IOError("Could not read data from Mi Flora sensor %s" % (self._mac))
+            raise IOError("Could not read data from Mi Flora sensor %s" % self._mac)
 
     def _check_data(self):
         if self._cache is None:
@@ -149,10 +150,20 @@ class MiFloraPoller(object):
             return None
 
     def _parse_data(self):
+        """Parses the byte array returned by the sensor.
+
+        The sensor returns 16 bytes in total. It's unclear what the meaning of these bytes
+        is beyond what is decoded in this method.
+        """
         data = self._cache
-        res = {}
+        res = dict()
         res[MI_TEMPERATURE] = float(data[1] * 256 + data[0]) / 10
         res[MI_MOISTURE] = data[7]
         res[MI_LIGHT] = data[4] * 256 + data[3]
         res[MI_CONDUCTIVITY] = data[9] * 256 + data[8]
         return res
+
+    @staticmethod
+    def _format_bytes(raw_data):
+        """Prettyprint a byte array."""
+        return ' '.join([format(c, "02x") for c in raw_data]).upper()
