4cca1a25e7e9dadd5727fe936c260278935eb92b
Daniel Matuschek <daniel@hifiberry.com>
2018-02-09 10:40:09 +0100

    Created release 0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='miflora',
-    version='0.2.1',
+    version='0.3',
     description='Library to read data from Mi Flora sensor',
     url='https://github.com/open-homeautomation/miflora',
     author='Daniel Matuschek',
