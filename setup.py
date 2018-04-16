4526cc9be241d4261e8541fde64ac281bdddd52f
Christian Kühnel <christian.kuehnel@gmail.com>
2018-04-16 21:00:29 +0200

    updated setup.py
    - added long description for pypi
    - updated status to "Production"
    - incremented version number

--- setup.py
+++ setup.py
@@ -1,16 +1,25 @@
 """Python package description."""
 from setuptools import setup, find_packages
 
+
+def readme():
+    """Load the readme file."""
+    with open('README.md') as readme_file:
+        return readme_file.read()
+
+
 setup(
     name='miflora',
-    version='0.3',
+    version='0.4',
     description='Library to read data from Mi Flora sensor',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
     url='https://github.com/open-homeautomation/miflora',
     author='Daniel Matuschek',
     author_email='daniel@matuschek.net',
     license='MIT',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: System :: Hardware :: Hardware Drivers',
         'License :: OSI Approved :: MIT License',
@@ -21,6 +30,6 @@ setup(
     packages=find_packages(),
     keywords='plant sensor bluetooth low-energy ble',
     zip_safe=False,
-    install_requires=['btlewrap==0.0.1'],
+    install_requires=['btlewrap==0.0.2'],
     extras_require={'testing': ['pytest']}
 )
