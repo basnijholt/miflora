8352a782a2fdc7ee77b134820c587c643299bc05
Christian Kühnel <christian.kuehnel@gmail.com>
2019-10-06 11:28:27 +0200

    added missing dependency on typing

--- setup.py
+++ setup.py
@@ -25,7 +25,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     packages=find_packages(),
     keywords='plant sensor bluetooth low-energy ble',
