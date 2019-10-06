f8c294d425bafb98073a2adf85ccd0c3b3b0bcc8
Christian Kühnel <christian.kuehnel@gmail.com>
2019-10-06 09:03:24 +0200

    upgraded to bluepy 1.3.0 and btlewrap 0.0.5
    fixed unittests
    improved run_integration_tests

--- miflora/miflora_poller.py
+++ miflora/miflora_poller.py
@@ -58,7 +58,7 @@ class MiFloraPoller:
         """
 
         self._mac = mac
-        self._bt_interface = BluetoothInterface(backend, adapter)
+        self._bt_interface = BluetoothInterface(backend)
         self._cache = None
         self._cache_timeout = timedelta(seconds=cache_timeout)
         self._last_read = None
