89b16bed3a1ad52df8e7003dfa11db44fd90a826
Christian Kühnel <christian.kuehnel@gmail.com>
2020-07-16 11:22:47 +0200

    fixed btlewrap version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     packages=find_packages(),
     keywords='plant sensor bluetooth low-energy ble',
     zip_safe=False,
-    install_requires=['btlewrap==0.0.9'],
+    install_requires=['btlewrap==0.0.10'],
     extras_require={'testing': ['pytest']},
     include_package_data=True,
 )
