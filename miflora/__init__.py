2e53ea76ef8760794d52298d335827d8c088c0d5
Bas Nijholt <bas@nijho.lt>
2020-09-08 16:10:55 +0200

    Fix that miflora actually works for Python 3.6

--- miflora/__init__.py
+++ miflora/__init__.py
@@ -1,15 +1,7 @@
 """Library to read data from Mi Flora sensor"""
 import sys
 
-# This check must be run first, so that it fails before loading the other modules.
-# Otherwise we do not get a clean error message.
-if sys.version_info <= (3, 6):
-    raise ValueError(
-        "this library requires at least Python 3.6. "
-        + "You're running version {}.{} from {}.".format(
-            sys.version_info.major, sys.version_info.minor, sys.executable
-        )
-    )
-from ._version import __version__  # pylint: disable=wrong-import-position
+assert sys.version_info >= (3, 6), "Miflora requires at least Python 3.6"
+from ._version import __version__  # noqa: E402, pylint: disable=wrong-import-position
 
 __all__ = ["__version__"]
