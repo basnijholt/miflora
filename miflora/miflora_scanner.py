15175afeaf4c18fc2099722cd3db034888dabb07
Bas Nijholt <bas@nijho.lt>
2020-09-08 14:49:07 +0200

    simplify scan

--- miflora/miflora_scanner.py
+++ miflora/miflora_scanner.py
@@ -11,12 +11,12 @@ def scan(backend, timeout=10):
 
     Note: this must be run as root!
     """
-    result = []
-    for (mac, name) in backend.scan_for_devices(timeout):
+    return [
+        mac.upper()
+        for (mac, name) in backend.scan_for_devices(timeout)
         if (
             (name is not None and name.lower() in VALID_DEVICE_NAMES)
             or mac is not None
             and mac.upper().startswith(DEVICE_PREFIX)
-        ):
-            result.append(mac.upper())
-    return result
+        )
+    ]
