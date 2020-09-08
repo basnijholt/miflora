dd794eb2ab2107123ca668f1b8eae201ebff4717
Bas Nijholt <bas@nijho.lt>
2020-09-08 16:28:40 +0200

    make error message more informative again

--- miflora/__init__.py
+++ miflora/__init__.py
@@ -1,7 +1,15 @@
 """Library to read data from Mi Flora sensor"""
 import sys
 
-assert sys.version_info >= (3, 6), "Miflora requires at least Python 3.6"
+# This check must be run first, so that it fails before loading the other modules.
+# Otherwise we do not get a clean error message.
+min_python_version = (3, 6)
+if sys.version_info < min_python_version:
+    raise ValueError(
+        "miflora requires Python≥{}.{}. You're running version {}.{} from {}.".format(
+            *min_python_version, *sys.version_info[:2], sys.executable
+        )
+    )
 from ._version import __version__  # noqa: E402, pylint: disable=wrong-import-position
 
 __all__ = ["__version__"]
