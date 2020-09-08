d5194deb386e83d99ac988cfbf11a421d98ce7e0
Bas Nijholt <basnijholt@gmail.com>
2020-09-08 10:18:16 +0200

    minimally require Python 3.6 and bump version to 0.7dev (#151)

--- miflora/__init__.py
+++ miflora/__init__.py
@@ -3,9 +3,9 @@ import sys
 
 # This check must be run first, so that it fails before loading the other modules.
 # Otherwise we do not get a clean error message.
-if sys.version_info <= (3, 4):
+if sys.version_info <= (3, 6):
     raise ValueError(
-        "this library requires at least Python 3.4. "
+        "this library requires at least Python 3.6. "
         + "You're running version {}.{} from {}.".format(
             sys.version_info.major, sys.version_info.minor, sys.executable
         )
